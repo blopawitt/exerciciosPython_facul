f0362664bb205eba9ebc5166f8e006c8b81ec313
Pablo Witt <88154429+blopawitt@users.noreply.github.com>
2022-01-18 20:46:56 -0300

    Update cap3_exercicio3.py

--- cap3_exercicio3.py
+++ cap3_exercicio3.py
@@ -11,6 +11,9 @@ if c == n:
 else:
     print("A ação falhou.")
 
+#Nesse exercício eu cometi um erro e recebi o feedback do professor:
+#"Ex3: não valida o 1 como número de Fibonacci, teste se código sempre com os valores extremos"
+#Para corrigir esse problema eu troquei o valor da variavel a para 0 e assim a troca de variaveis acontece corretamente e o valor 1 é incluso.
 
 
 
